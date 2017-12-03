0c73db74df07c6fc0f43c0dfdceb61cec47dd36a
Thiago Holanda <unnamedd@gmail.com>
2017-12-03 12:13:32 +0100

    implemented the change of colors on loop

--- semaphore-arduino.ino
+++ semaphore-arduino.ino
@@ -12,24 +12,36 @@ enum STATUS {
   SUCCESS
 };
 
+// Life cycle
 void setup() {
+  setupBuzzer();
   setupFirstLed();
   setupSecondLed();
 }
 
 void loop() {
+//  tone(buzzerPin, 1000);
+//  delay(300);
+//  noTone(buzzerPin);
+//  delay(3000);
+  
+  setStatus(firstLed, running);
+  delay(2000);
+  
+  setStatus(firstLed, success);
+  delay(2000);
 
+  setStatus(firstLed, failure);
+  delay(2000);
 }
 
+// Set colors
 void setColor(int redPin, int redValue, int greenPin, int greenValue, int bluePin, int blueValue) {
   analogWrite(redPin, redValue);
   analogWrite(greenPin, greenValue);
   analogWrite(bluePin, blueValue);
 }
 
-// Set color into RGB LEDs
-void setYellowCollor(int redPin, int greenPin, int bluePin) {
-  setColor(redPin, 255, greenPin, 255, bluePin, 0);
 void setRunningColor(int redPin, int greenPin, int bluePin) {
   setColor(redPin, 255, greenPin, 160, bluePin, 0);
 }
