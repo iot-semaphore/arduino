f4ee8db46869115bdf4555c1de9bb1d314f5f1e3
Thiago Holanda <unnamedd@gmail.com>
2017-12-03 05:11:11 +0100

    initial commit from semaphore on arduino

---
+++ README.md
@@ -0,0 +1,7 @@
+# Semaphore Arduino
+
+This project is part of the [Semaphore](https://github.com/unnamedd/semaphore).
+
+
+
+ 
---
+++ semaphore-arduino.ino
@@ -0,0 +1,54 @@
+int firstRedPin   = A1;
+int firstGreenPin = A2;
+int firstBluePin  = A3;
+
+int secondRedPin   = A4;
+int secondGreenPin = A5;
+int secondBluePin  = A6;
+
+enum STATUS {
+  RUNNING,
+  FAILURE,
+  SUCCESS
+};
+
+void setup() {
+  setupFirstLed();
+  setupSecondLed();
+}
+
+void loop() {
+
+}
+
+void setColor(int redPin, int redValue, int greenPin, int greenValue, int bluePin, int blueValue) {
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin, greenValue);
+  analogWrite(bluePin, blueValue);
+}
+
+// Set color into RGB LEDs
+void setYellowCollor(int redPin, int greenPin, int bluePin) {
+  setColor(redPin, 255, greenPin, 255, bluePin, 0);
+}
+
+void setRedCollor(int redPin, int greenPin, int bluePin) {
+  setColor(redPin, 255, greenPin, 0, bluePin, 0);
+}
+
+void setGreenCollor(int redPin, int greenPin, int bluePin) {
+  setColor(redPin, 0, greenPin, 255, bluePin, 0);
+}
+
+// Setup LEDs
+void setupFirstLed() {
+  pinMode(firstRedPin, OUTPUT);
+  pinMode(firstGreenPin, OUTPUT);
+  pinMode(firstBluePin, OUTPUT);
+}
+
+void setupSecondLed() {
+  pinMode(secondRedPin, OUTPUT);
+  pinMode(secondGreenPin, OUTPUT);
+  pinMode(secondBluePin, OUTPUT);
+}
