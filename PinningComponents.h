309a48afeb8c2411e06613994c31133ce72938e1
Thiago Holanda <unnamedd@gmail.com>
2017-12-05 00:32:32 +0100

    created a header to configure the pins

---
+++ PinningComponents.h
@@ -0,0 +1,11 @@
+// Pin settings
+int firstRedPin = 3;
+int firstGreenPin = 4;
+int firstBluePin = 5;
+
+int secondRedPin = 10;
+int secondGreenPin = 9;
+int secondYellowPin = 8;
+
+int buzzerPin = 6;
+int buttonPin = 7;
