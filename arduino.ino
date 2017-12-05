f82f862da3f56e66ef5badb8f4cc558a2de9fd73
Thiago Holanda <unnamedd@gmail.com>
2017-12-05 02:58:34 +0100

    implemented the http request

--- arduino.ino
+++ arduino.ino
@@ -2,12 +2,22 @@
 #include "PinningComponents.h"
 #include "Enums.h"
 
+// External libraries
+#include <SPI.h>
+#include <WiFi101.h>
+#include <ArduinoJson.h>
 
 bool beeping = false;
 
+char ssid[] = ""; // WiFi Network name
+char pass[] = ""; // WiFi password
 
+int keyIndex = 0;
+int wireless_status = WL_IDLE_STATUS;
 
+char server[] = "semaphore.vapor.cloud";
 
+WiFiClient client;
 
 // Life cycle
 void setup() {
@@ -15,27 +25,101 @@ void setup() {
   setupBuzzer();
   setupFirstLed();
   setupSecondLed();
+
+  Serial.begin(9600);
+  while(!Serial){};
+
+  while (wireless_status != WL_CONNECTED) {
+    echo("Attempting to connect to SSID: ");
+    echoln(ssid);
+
+    wireless_status = WiFi.begin(ssid, pass);
+    delay(5000);
+  }
+
+  echo("Connected to WiFi: ");
+  echoln(ssid);
+  
+  long signal_strengh = WiFi.RSSI();
+  echo("Signal strengh: ");
+  Serial.print(signal_strengh);
+  echoln(" dBm");
+  echoln("################################");
 }
 
 void loop() {
-//  beeping = true;
-//  tone(buzzerPin, 1000);
-//  delay(300);
-//  noTone(buzzerPin);
-//  delay(3000);
+  // Connecting to server
+  client.connect(server, 80) || die("Connection failed");
+  echo("Connected to ");
+  echoln(server);
 
+  // Requesting data from endpoint '/'
+  client.println("GET / HTTP/1.1");
+  client.print("Host: ");
+  client.println(server);
+  client.println("Connection: close");
+  client.println() || die("Failed to request data");
 
-  setStatus(firstLed, running);
-  setStatus(secondGroup, running);
-  delay(500);
-  
-  setStatus(firstLed, success);
-  setStatus(secondGroup, success);
-  delay(500);
+  if (client) {
+    bool success = readRequest(client);
+    if (success) {
+      const size_t BUFFER_SIZE = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+      DynamicJsonBuffer jsonBuffer(BUFFER_SIZE);
 
-  setStatus(firstLed, failure);
-  setStatus(secondGroup, failure);
-  delay(500);
+      JsonArray &root = jsonBuffer.parseArray(client);
+
+      if (!root.success())
+        die("Parsing failed");
+
+      echo("Extract values: ");
+      Serial.println(root.size());
+      echoln("##########################################");
+      
+      int jobsLength = root.size();
+      for (int i = 0; i < jobsLength; i++) {
+        JsonObject &job = root[i];
+        String _name = job.get<String>("name");
+        String _status = job.get<String>("status");
+        status jobStatus = getStatus(_status);
+        
+        if (_name == "example1") {
+          setStatus(firstLed, jobStatus);
+        }
+        else if (_name == "example2") {
+          setStatus(secondGroup, jobStatus);
+        }
+        
+        echo("Job name: ");
+        Serial.println(_name);
+        echo("Status: ");
+        Serial.println(_status);
+        echoln("##########################################");
+      }
+
+      echoln("##########################################");
+      echoln("Waiting 10 seconds before the next request");
+      echoln("##########################################");
+      delay(10000);
+    }
+  }
+}
+
+bool readRequest(WiFiClient &client) {
+  bool currentLineIsBlank = true;
+  while (client.connected()) {
+    if (client.available()) {
+      char c = client.read();
+
+      if (c == '\n' && currentLineIsBlank)
+        return true;
+      else if (c == '\n')
+        currentLineIsBlank = true;
+      else if (c != '\r')
+        currentLineIsBlank = false;
+    }
+  }  
+
+  return false;
 }
 
 // RGB LED set colors
@@ -57,8 +141,20 @@ void setSuccessColor(int redPin, int greenPin, int bluePin) {
   setColor(redPin, 0, greenPin, 255, bluePin, 0);
 }
 
+status getStatus(String value) {
+  if (value == "RUNNING")
+    return running;
+  
+  else if (value == "SUCCESS")
+    return success;
+
+  else
+    return failure;
+}
+
 void setStatus(components pin, status status) {
-  // as we are using one RGB Led and three colored Leds, 
+  echoln("setStatus called");
+  // as we are using one RGB Led and three colored Leds,
   // we need to do this early return
   if (pin == secondGroup) {
     if (status == running) {
@@ -83,7 +179,7 @@ void setStatus(components pin, status status) {
 
   // Continue only if the component selected is the first, but
   // we still enable to add more RGB LEDs and set else statement
-  
+
   int redPin;
   int greenPin;
   int bluePin;
@@ -125,4 +221,20 @@ void setupSecondLed() {
   pinMode(secondRedPin, OUTPUT);
   pinMode(secondGreenPin, OUTPUT);
   pinMode(secondYellowPin, OUTPUT);
+}
+
+// Utils (thanks ArduinoJSON)
+void echoln(const char *message) {
+  Serial.println(message);  
+}
+
+void echo(const char *message) {
+  Serial.print(message);  
+}
+
+bool die(const char *message) {
+  echo(message);
+  while(true);
+
+  return false;
 }
